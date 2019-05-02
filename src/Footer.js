20164a8e083cb019eba63fb671efbcc300915912
mhdiriansyah <muhammad.iriansyah@ralali.com>
2019-05-02 15:16:00 +0700

    baru update

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <div>
+        <h3>Halaman Footer</h3>
+        <p>@2018</p>
+      </div>
+    );
+}
+
+export default Footer;
